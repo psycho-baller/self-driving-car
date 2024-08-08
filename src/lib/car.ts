c5f27b7b34bb715b465b6610df3d0d9374f3fef4
Rami Maalouf <Ramim66809@gmail.com>
2024-08-08 14:49:52 +0400

    rotate less

--- src/lib/car.ts
+++ src/lib/car.ts
@@ -157,10 +157,10 @@ export class Car {
     if (this.speed != 0) {
       const flip = this.speed > 0 ? 1 : -1;
       if (this.controls.left) {
-        this.angle += 0.03 * flip;
+        this.angle += 0.01 * flip;
       }
       if (this.controls.right) {
-        this.angle -= 0.03 * flip;
+        this.angle -= 0.01 * flip;
       }
     }
 
